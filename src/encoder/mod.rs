f353c7814a2c81e1c7f5544be7c70b7edd6fab7b
Robert Wassens <robert.wassens@technolution.nl>
2020-12-02 12:05:25 +0100

    store unlimited strip size

--- src/encoder/mod.rs
+++ src/encoder/mod.rs
@@ -581,10 +581,11 @@ impl<'a, W: 'a + Write + Seek, T: ColorType> ImageEncoder<'a, W, T> {
         height: u32,
     ) -> TiffResult<ImageEncoder<'a, W, T>> {
         let row_samples = u64::from(width) * u64::try_from(<T>::BITS_PER_SAMPLE.len())?;
-        let row_bytes = row_samples * u64::from(<T::Inner>::BYTE_LEN);
+        // let row_bytes = row_samples * u64::from(<T::Inner>::BYTE_LEN);
 
         // As per tiff spec each strip should be about 8k long
-        let rows_per_strip = (8000 + row_bytes - 1) / row_bytes;
+        // let rows_per_strip = (8000 + row_bytes - 1) / row_bytes;
+        let rows_per_strip = height as u64;
 
         let strip_count = (u64::from(height) + rows_per_strip - 1) / rows_per_strip;
 
